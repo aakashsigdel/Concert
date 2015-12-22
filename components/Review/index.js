676c91c0b5c61b6aa7c67cdaee2e550f17e85c7f
nabn <nabeen.khadka@gmail.com>
2015-12-22 12:07:02 +0545

    - make click area larger on review page; - implement like feature

--- components/Review/index.js
+++ components/Review/index.js
@@ -31,6 +31,9 @@ export default class Review extends Component {
     this.state = {
       renderPlaceholderOnly: true,
       isLoading: false,
+      isLiked: false,
+      likeCount: 328,
+      heartImage: require('../../assets/images/like.png'),
     };
   }
 
@@ -56,6 +59,14 @@ export default class Review extends Component {
         isLoading: false,
       });
     });
+  }
+
+  _toggleLike() {
+    this.setState({
+      isLiked: !this.state.isLiked,
+      heartImage: this.state.isLiked?  require('../../assets/images/like.png' ) : require('../../assets/images/liked.png'),
+      likeCount: this.state.isLiked? 328 : 329,
+    })
   }
 
 	// TODO: this function should be in a global module
@@ -146,32 +157,33 @@ export default class Review extends Component {
 
         <View style={comment.container} > 
           <View style={comment.header} >
-            <Image
-              style={comment.starImage}
-              source={require('../../assets/images/userpicCopy.png')}
-            />
-            <View style={comment.headerText}>
-              <TouchableHighlight
-                onPress={this._handleUserPress.bind(this, 1, 'JIMMI ANDERSEN')}
-                >
+            <TouchableOpacity
+              onPress={this._handleUserPress.bind(this, 1, 'JIMMI ANDERSEN')}
+              style={{flex: 1, flexDirection: 'row'}}>
+              <Image
+                style={comment.starImage}
+                source={require('../../assets/images/userpicCopy.png')}
+              />
+              <View style={comment.headerText}>
                 <Text style={comment.whiteText} >JIMMI ANDERSEN</Text>
-              </TouchableHighlight>
-              <View style={header.ratingStars} >
-                {this._getStars(3)}
+                <View style={header.ratingStars} >
+                  {this._getStars(3)}
+                </View>
               </View>
-            </View>
+            </TouchableOpacity>
 
-            <View
-              style={comment.starContainer} >
+            <TouchableOpacity
+              onPress={this._toggleLike.bind(this)}
+              style={comment.starContainer}>
               <Image 
-                source={require('../../assets/images/like.png')}
+                source={this.state.heartImage}
                 style={comment.likeImage}
               />
               <Text
                 style={comment.likesText}>
-                329 LIKES
+                {this.state.likeCount} LIKES
               </Text>
-            </View>
+            </TouchableOpacity>
 
 
           </View>
